c69da04cd61bdfe5e238905d35a639ff24bd4795
CogBrainHealthLab <148365456+CogBrainHealthLab@users.noreply.github.com>
2026-06-08 12:01:18 +0800

    Update CIFTItoFC.R

--- R/CIFTItoFC.R
+++ R/CIFTItoFC.R
@@ -29,7 +29,7 @@
 
 ########################################################################################################
 ########################################################################################################
-CIFTItoFC=function(path="./",wb_path="/home/junhong.yu/workbench/bin_rh_linux64", dtseries="_space-fsLR_den-91k_desc-denoised_bold.dtseries.nii", timeseries=F, concat_subj=TRUE, atlas=200, round,filename="FC.rds")
+CIFTItoFC=function(path="./",wb_path="/home/junhong.yu/workbench/bin_rh_linux64", dtseries="_space-fsLR_den-91k_desc-denoised_bold.dtseries.nii", timeseries=F, concat_subj=TRUE, atlas=200, round.dat=3,filename="FC.rds")
 {
   filelist=list.files(path=path,pattern=dtseries,recursive = T)
   sublist=unique(gsub(pattern ="/ses-.*/func",replacement = "",dirname(filelist)))
